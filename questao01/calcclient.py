de587e68eafb044c0a1a8d1a37720aa4be1d8c87
Andrea Elias <andreaase@hotmail.com>
2022-10-01 20:54:06 -0300

    tratamento calculadora

--- questao01/calcclient.py
+++ questao01/calcclient.py
@@ -1,4 +1,5 @@
 import socket
+import re
 
 IP = "127.0.0.1"
 PORT = 5000
@@ -12,20 +13,73 @@ while True:
     print('Insira o 1o valor')
     a = input()
 
+    if(a == 'X' or a == 'x'):
+        message = 'sair'
+        socket.sendto(message.encode('utf-8'), (IP, PORT))
+        break
+
+    while True:
+        try:
+            float(a)
+        
+        except ValueError:
+            # Not a valid number
+            print("\nO valor deve ser um número!\n")
+            a = input("Insira o 1o valor: ")
+        else:
+            break
+    
+    if(a == 'X' or a == 'x'):
+        message = 'sair'
+        socket.sendto(message.encode('utf-8'), (IP, PORT))
+        break
+
+
     print('Insira o 2o valor')
     b = input()
+
+    if(b == 'X' or b == 'x'):
+        message = 'sair'
+        socket.sendto(message.encode('utf-8'), (IP, PORT))
+        break
+
+    while True:
+        try:
+            float(b)
+        
+        except ValueError:
+            # Not a valid number
+            print("\nO valor deve ser um número!\n")
+            b = input("Insira o 2o valor: ")
+        else:
+            break
+    
+    if(b == 'X' or b == 'x'):
+        message = 'sair'
+        socket.sendto(message.encode('utf-8'), (IP, PORT))
+        break
 
     print('Insira o operador, + para soma, - para subtração, * para multiplicação e / para divisão:')
     operator = input()
 
+    if(operator == 'X' or operator == 'x'):
+        message = 'sair'
+        socket.sendto(message.encode('utf-8'), (IP, PORT))
+        break
 
+    while(operator!='+' and operator!='-' and operator!='*' and operator!='/'):
+        operator = input('\n O operador digitado não é valido. \n Insira o operador, + para soma, - para subtração, * para multiplicação e / para divisão:')
+
+    if(operator == 'X' or operator == 'x'):
+        message = 'sair'
+        socket.sendto(message.encode('utf-8'), (IP, PORT))
+        break
 
     message = a +', '+b+', '+operator
     print('Mensagem recebida do servidor: ' + message + "\n")
 
     socket.sendto(message.encode('utf-8'), (IP, PORT))
-    if a == 'X' or b == 'X' or operator == 'X':
-        break
+   
 
     data, address = socket.recvfrom(2048)
     text = data.decode('utf-8')
--- questao01/calcserver.py
+++ questao01/calcserver.py
@@ -11,12 +11,25 @@ socket.bind((IP, PORT))
 
 print('Server Listening At {}'.format(socket.getsockname()))
 
+def valorinteiro(v):
+  n = int(v)
+  m = v * 10
+  s = n * 10
+  if m == s:
+    return True
+  else:
+    return False
+
 while True:
     messageBytes, address = socket.recvfrom(2048)
     messageString = messageBytes.decode('utf-8')
     print('Recebido do cliente {} : {}'.format(address, messageString))
 
+    k = messageString
+    if k=='sair':
+        break
     messageString = messageString.split(", ")
+    
     a = messageString[0]
     b = messageString[1]
     operator = messageString[2]
@@ -26,22 +39,30 @@ while True:
 
     if operator == '+':
         print("soma")
-        result = int(a) + int(b)
+        result = float(a) + float(b)
+        if (valorinteiro(result) == True):
+            result = int(result)
         print("Resultado:", result)
 
     elif operator == '-':
         print("subtração")
-        result = int(a) - int(b)
+        result = float(a) - float(b)
+        if (valorinteiro(result) == True):
+            result = int(result)
         print("Resultado:", result)
     
     elif operator == '*':
         print("multiplicação")
-        result = int(a) * int(b)
+        result = float(a) * float(b)
+        if (valorinteiro(result) == True):
+            result = int(result)
         print("Resultado:", result)
     
     elif operator == '/':
         print("divisão")
-        result = int(a) / int(b)
+        result = float(a) / float(b)
+        if (valorinteiro(result) == True):
+            result = int(result)
         print("Resultado:", result)
 
 
