a5ca593a41dc3aa086fb51e0028ebbaa95ee19a7
John Vasconcelos <39773960+Johnvasc@users.noreply.github.com>
2022-10-02 21:03:52 -0300

    Update iotGadget2.py

--- questao3/iotGadget2.py
+++ questao3/iotGadget2.py
@@ -7,12 +7,12 @@ HOST = 'localhost'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # mensagem de identificação do gadget ao servidor
 s.connect((HOST, PORT))
-msg = 'iotGadget'
+msg = 'iotGadget_ID:1235_TY:PassNA:gadgetUmidade'
 s.sendall(bytes(msg, 'utf-8'))
-#funcionamento do gadget de sensor contínuo, da umidade entre 60 e 70% e dorme por 15s
+#funcionamento do gadget de sensor contínuo, da umidade entre 60 e 70% e dorme por 8s
 while True:
-    temp = random.randrange(60, 70)
-    msg = f"umidade: {temp}%"
+    umid = random.randrange(60, 70)
+    msg = f"umidade: {umid}%"
     print(msg)
     s.sendall(bytes(msg, 'utf-8'))
-    time.sleep(15)
+    time.sleep(8)
