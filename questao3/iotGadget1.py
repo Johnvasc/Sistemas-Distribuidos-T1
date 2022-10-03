80926ce037a14bc9e6d5c2ae4b99d9f0cf7ff7ff
John Vasconcelos <39773960+Johnvasc@users.noreply.github.com>
2022-10-02 21:03:22 -0300

    Update iotGadget1.py

--- questao3/iotGadget1.py
+++ questao3/iotGadget1.py
@@ -7,9 +7,9 @@ HOST = 'localhost'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # mensagem de identificação do gadget ao servidor
 s.connect((HOST, PORT))
-msg = 'iotGadget'
+msg = 'iotGadget_ID:1234_TY:Pass_NA:sensorTemperatura'
 s.sendall(bytes(msg, 'utf-8'))
-#funcionamento do gadget de sensor contínuo, da temperaturas entre 30 e 40º ceusius e dorme por 5s
+#funcionamento do gadget de sensor contínuo, de temperaturas entre 30 e 40º ceusius e dorme por 5s
 while True:
     temp = random.randrange(30, 40)
     msg = f"temperatura: {temp}º ceusius"
